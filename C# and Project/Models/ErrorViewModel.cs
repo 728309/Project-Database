e3f47e3aa93e1a8d44c897b4e73b4914cf6fbf78
728309 <728309@student.inholland.nl>
2025-03-11 13:30:38 +0100

    Add project files.

---
+++ C# and Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C__and_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
