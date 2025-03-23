4d798a4fc10ee360284efe41e80a1863d1d98992
728309 <728309@student.inholland.nl>
2025-03-23 11:45:46 +0100

    I hope this is good, just the database is not working

---
+++ C# and Project/Repositories/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using C__and_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C__and_Project.Repositories
+{
+    public interface IStudentRepository
+    {
+        List<Student> GetAllStudents();
+        Student? GetStudentByID(int studentID);
+        void AddStudent(Student student);
+        void UpdateStudent(Student student);
+        void DeleteStudent(Student student);
+    }
+}
