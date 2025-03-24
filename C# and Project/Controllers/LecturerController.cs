a4def00e51cc0084f46eb0f6904dd004004eca24
Kirya-Inh <728067@student.inholland.nl>
2025-03-24 03:06:53 +0100

    Added lecturer controller

---
+++ C# and Project/Controllers/LecturerController.cs
@@ -0,0 +1,13 @@
+﻿using C__and_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C__and_Project.Controllers
+{
+    public class LecturerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
