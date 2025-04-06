bac295ba8d3bb419e14cd9e339d9eb15de2a4974
728287Luwana <728287@student.inholland.nl>
2025-04-06 17:02:26 +0200

    Supervise

---
+++ C# and Project/Models/Supervise.cs
@@ -0,0 +1,17 @@
+﻿namespace C__and_Project.Models
+{
+    public class Supervise
+    {
+        public int ActivityID { get; set; }
+        public int LecturerID { get; set; }
+
+        public Supervise() { }
+
+        public Supervise(int activityID, int lecturerID)
+        {
+            ActivityID = activityID;
+            LecturerID = lecturerID;
+        }
+
+    }
+}
