5f731b07ce192acad316b28fac261798d3cb24e3
728287Luwana <728287@student.inholland.nl>
2025-04-06 17:41:24 +0200

    ManageParticipants

---
+++ C# and Project/Views/Activity/ManageParticipants.cs
@@ -0,0 +1,11 @@
+﻿using C__and_Project.Models;
+
+namespace C__and_Project.Views.Activity
+{
+    public class ManageParticipants
+    {
+        public Activity Activity { get; set; }
+        public List<Student> Participants { get; set; }
+        public List<Student> AvailableStudents { get; set; }
+    }
+}
